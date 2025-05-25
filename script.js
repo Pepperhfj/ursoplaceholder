10c0d712571e53c496623c6f5fa7dea143a9c732
CollectionofEG <148367269+Pepperhfj@users.noreply.github.com>
2025-05-25 08:26:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+  // Prints dice roll to the page ! 
+  function printNumber(number) {
+    var placeholder = document.getElementById('placeholder'); // Gets the element Id of 'placeholder' !
+    placeholder.innerHTML = number;
+  }
+  
+  var dice = {
+    sides: ["epic!", "awesome!", "cool!"], // The array of words that get printed to the html (^o^)
+    roll: function () {
+      var randomIndex = Math.floor(Math.random() * this.sides.length); // Gets a random index (O_o)
+      return this.sides[randomIndex]; // Returns the words at that index !
+    }
+  };
+  
+  var button = document.getElementById('button'); // Gets the element ID of 'button' !
+  
+  button.onclick = function() {
+    var result = dice.roll();
+    printNumber(result);
+  };
+
+
+
+  
